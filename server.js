9f60d7948b7038a1f58d2a90f6077be6638069ae
Saikiran Bimbadhar <sai.406@gmail.com>
2016-09-18 17:33:01 +0530

    errors updated in server

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-  res.send(createTemplate(articles(articleName)));
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
