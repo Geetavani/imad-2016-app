14dc1495eb5089680f7ad1c9a2e2c6374d5b1b18
Saikiran Bimbadhar <sai.406@gmail.com>
2016-09-18 16:52:48 +0530

    code changed in serverlease enter the commit message for your changes. Lines starting

--- server.js
+++ server.js
@@ -5,12 +5,36 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+title:'article-one',
+heading:'First Heading',
+content:`<p> This html contains article one data</p>`	
+};
+
+function createTemplate(data){
+	var title=data.title;
+	var heading=data.heading;
+	var content=data.content;
+	var htmlTemplate=`<html>
+		<title>{$title}</title>
+		<head>
+			${heading}
+		</head>
+		<body>
+			${content}
+		</body>
+
+	</html>
+	`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleone));
 });
 
 app.get('/ui/style.css', function (req, res) {
